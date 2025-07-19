d41e22eba478fef876ac778b40beb9568e9f5aba
HIBIKI_5201 <takuto5201@gmail.com>
2025-07-19 23:40:22 +0900

    [add]アイテムのスポーン位置のクラスを追加

---
+++ Assets/Scripts/Runtime/Ingame/Stage/ItemSpawnPoint.cs
@@ -0,0 +1,71 @@
+using ChristianGamers.Ingame.Item;
+using System;
+using System.Linq;
+using UnityEngine;
+
+namespace ChristianGamers
+{
+    public class ItemSpawnPoint : MonoBehaviour
+    {
+        [SerializeField]
+        private SpawnItem[] _spawnItems;
+
+        private void Awake()
+        {
+            if (_spawnItems == null || _spawnItems.Length == 0)
+            {
+                Debug.LogWarning("Spawn items are not configured. Please check the configuration.");
+                return;
+            }
+
+            //ランダムなスポーンアイテムを取得
+            SpawnItem item = GetRandomSpawnItem();
+            if (item.item == null)
+            {
+                Debug.LogError("SpawnItem is null. Please check the configuration.");
+                return;
+            }
+
+            //Pivotの量だけオフセットを掛ける
+            Vector3 spawnPosition = transform.TransformPoint(item.item.SpawnPivot.localPosition);
+            // アイテムをスポーンポイントの位置に生成
+            Instantiate(item.item, spawnPosition, Quaternion.identity);
+
+            Destroy(gameObject); // スポーンポイント自体は不要なので削除
+        }
+
+        /// <summary>
+        ///     重さ確率でランダムなスポーンアイテムを取得する
+        /// </summary>
+        /// <returns></returns>
+        private SpawnItem GetRandomSpawnItem()
+        {
+            int sumWeight = _spawnItems.Sum(e => e.spawnWeight);
+            int randomWeight = UnityEngine.Random.Range(0, sumWeight);
+
+            int currentWeight = 0;
+            for (int i = 0; i < _spawnItems.Length; i++)
+            {
+                if (randomWeight < _spawnItems[i].spawnWeight + currentWeight)
+                {
+                    return _spawnItems[i];
+                }
+
+                // 現在のアイテムの重みを加算
+                currentWeight += _spawnItems[i].spawnWeight;
+            }
+
+            return default;
+        }
+
+        /// <summary>
+        ///     スポーンアイテムの構造体
+        /// </summary>
+        [Serializable]
+        private struct SpawnItem
+        {
+            public ItemBase item;
+            public int spawnWeight;
+        }
+    }
+}
