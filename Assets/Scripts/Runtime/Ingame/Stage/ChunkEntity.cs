e988f932b661365eff1b3a8740e36080dfb59a2c
HIBIKI_5201 <takuto5201@gmail.com>
2025-07-19 18:23:53 +0900

    [add]チャンクのエンティティクラスを追加

---
+++ Assets/Scripts/Runtime/Ingame/Stage/ChunkEntity.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace ChristianGamers.Ingame.Stage
+{
+    /// <summary>
+    ///     チャンクのエンティティを表すクラス
+    /// </summary>
+    public class ChunkEntity : MonoBehaviour
+    {
+
+    }
+}
