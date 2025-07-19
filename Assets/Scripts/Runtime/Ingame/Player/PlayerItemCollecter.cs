04d03e65df3a2444f832c5ad2484378c0270dc2a
HIBIKI_5201 <takuto5201@gmail.com>
2025-07-19 15:12:55 +0900

    [update]範囲内のアイテムを探して取得するメソッドを追加

--- Assets/Scripts/Runtime/Ingame/Player/PlayerItemCollecter.cs
+++ Assets/Scripts/Runtime/Ingame/Player/PlayerItemCollecter.cs
@@ -1,12 +1,46 @@
+using ChristianGamers.Ingame.Item;
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace ChristianGamers.Ingame.Player
 {
     /// <summary>
     ///     プレイヤーがアイテムを収集するためのクラス。
-    /// </summary>
+    /// </summary>]
+    [Serializable]
     public class PlayerItemCollecter
     {
+        /// <summary>
+        ///     範囲内のアイテムを取得する
+        /// </summary>
+        /// <param name="selfPos"></param>
+        /// <param name="range">回収半径</param>
+        /// <param name="angleThreshold">回収範囲（度数）</param>
+        /// <returns></returns>
+        public ItemBase[] GetItems(Transform selfPos, float range, float angleThreshold)
+        {
+            //範囲内のオブジェクトを取得
+            Collider[] hits = Physics.OverlapSphere(selfPos.position, range);
 
+            List<ItemBase> result = new List<ItemBase>();
+            foreach (Collider hit in hits)
+            {
+                //アイテム以外は無視
+                if (!hit.TryGetComponent(out ItemBase item)) continue;
+
+                // 角度を計算
+                Vector3 directionToItem = hit.transform.position - selfPos.position;
+                float angle = Vector3.Angle(selfPos.forward, directionToItem);
+
+                // 角度が閾値以下なら収集可能
+                if (angle <= angleThreshold)
+                {
+                    result.Add(item);
+                }
+            }
+
+            return result.ToArray();
+        }
     }
 }
