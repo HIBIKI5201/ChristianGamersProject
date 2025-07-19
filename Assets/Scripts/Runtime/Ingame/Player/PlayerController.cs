6c1efdc95617744ff9a2f9013aee458e1b3f4897
HIBIKI_5201 <takuto5201@gmail.com>
2025-07-19 12:59:50 +0900

    [add]PlayerControllerを追加

---
+++ Assets/Scripts/Runtime/Ingame/Player/PlayerController.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+namespace ChristianGamers
+{
+    public class PlayerController
+    {
+        private Transform _self;
+
+        public PlayerController(Transform self)
+        {
+            _self = self;
+        }
+
+    }
+}
