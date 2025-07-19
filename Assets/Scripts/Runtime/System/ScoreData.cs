f6bb41df02792d8998288026c6e68fceb6c5c51c
HIBIKI_5201 <takuto5201@gmail.com>
2025-07-20 01:28:32 +0900

    [add]スコアのデータクラスを追加

---
+++ Assets/Scripts/Runtime/System/ScoreData.cs
@@ -0,0 +1,24 @@
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace ChristianGamers.System.Score
+{
+    /// <summary>
+    ///     スコアのデータクラス
+    /// </summary>
+    public class ScoreData
+    {
+        public void AddScore(int score)
+        {
+            _scores.Add(score);
+            Debug.Log($"[ScoreData] スコア追加: {score}（現在のスコア数: {_scores.Count}）");
+
+            _scores = _scores.OrderBy(x => x).Take(MAX_SCORES).ToList(); // 降順にソート
+        }
+
+        private const int MAX_SCORES = 5;
+
+        private List<int> _scores = new();
+    }
+}
