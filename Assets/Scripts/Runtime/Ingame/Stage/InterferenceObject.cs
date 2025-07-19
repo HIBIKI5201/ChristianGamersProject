881ecfeb614381a69b8b2be11aa981db2857b9b1
HIBIKI_5201 <takuto5201@gmail.com>
2025-07-20 02:19:53 +0900

    [update]ノックバックを正式実装

--- Assets/Scripts/Runtime/Ingame/Stage/InterferenceObject.cs
+++ Assets/Scripts/Runtime/Ingame/Stage/InterferenceObject.cs
@@ -1,5 +1,7 @@
 using ChristianGamers.Ingame.Player;
+using ChristianGamers.Utility;
 using SymphonyFrameWork.System;
+using UnityEditor.AddressableAssets.Build;
 using UnityEngine;
 
 namespace ChristianGamers.Ingame.Stage
@@ -7,14 +9,19 @@ namespace ChristianGamers.Ingame.Stage
     /// <summary>
     /// 妨害オブジェクト
     /// </summary>
+    [RequireComponent(typeof(Rigidbody))]
     public class InterferenceObject : MonoBehaviour
     {
         [SerializeField]
         private float _knockBackPower = 5;
+        [SerializeField]
+        private float _stunTime = 1f; // ノックバック中のスタン時間
 
         private void OnTriggerEnter(Collider other)
         {
-            if (other.gameObject.TryGetComponent<PlayerManager>(out PlayerManager player))
+            PlayerManager player = TransformUtility.FindTypeByParents<PlayerManager>(other.transform);
+
+            if (player != null)
             {
                 if (player.IsInvincible)
                 {
@@ -23,9 +30,8 @@ namespace ChristianGamers.Ingame.Stage
                 }
                 else
                 {
-                     Vector3 closestPoint = other.ClosestPoint(transform.position); // 近い位置を取得
-                    Vector3 dir = (closestPoint - transform.position).normalized; // 方向を計算
-                    player.KnockBack(dir, _knockBackPower);
+                    Vector3 dir = (other.transform.position - transform.position).normalized; // 方向を計算
+                    player.KnockBack(dir * _knockBackPower, _stunTime);
                 }
             }
         }
