88a219084e4c9200f4c30c9cc170440bc9affe9c
HIBIKI_5201 <takuto5201@gmail.com>
2025-07-20 14:58:23 +0900

    [update]死亡イベントを追加

--- Assets/Scripts/Runtime/Ingame/Stage/InterferenceObject.cs
+++ Assets/Scripts/Runtime/Ingame/Stage/InterferenceObject.cs
@@ -1,8 +1,11 @@
 using ChristianGamers.Ingame.Player;
 using ChristianGamers.Utility;
-using SymphonyFrameWork.System;
-using UnityEditor.AddressableAssets.Build;
+using Action = System.Action;
+using Unity.Behavior;
 using UnityEngine;
+using System;
+using UnityEngine.AI;
+using Unity.VisualScripting;
 
 namespace ChristianGamers.Ingame.Stage
 {
@@ -12,10 +15,19 @@ namespace ChristianGamers.Ingame.Stage
     [RequireComponent(typeof(Rigidbody))]
     public class InterferenceObject : MonoBehaviour
     {
-        [SerializeField]
+        public event Action OnRespawn;
+        public event Action OnDead;
+
+        [SerializeField, Tooltip("ノックバックの吹っ飛び力")]
         private float _knockBackPower = 5;
-        [SerializeField]
+        [SerializeField, Tooltip("プレイヤーをスタンさせる時間")]
         private float _stunTime = 1f; // ノックバック中のスタン時間
+
+        [SerializeField, Tooltip("リスポーンするまでの時間")]
+        private float _respawnTime;
+
+        [SerializeField, Tooltip("Agentの")]
+        private string _BBPatrolPoints = "Patrol Points";
 
         private void OnTriggerEnter(Collider other)
         {
@@ -25,8 +37,34 @@ namespace ChristianGamers.Ingame.Stage
             {
                 if (player.IsInvincible)
                 {
-                    Destroy(gameObject);
-                    //壊れるAnimationの生成など
+                    if (!TryGetComponent(out BehaviorGraphAgent agent)) return;
+                    if (!TryGetComponent(out Rigidbody rb)) return;
+                    if (!TryGetComponent(out NavMeshAgent nvAgent)) return;
+
+                    //プレイヤーに当たらないように変更
+                    rb.excludeLayers = ~LayerMask.NameToLayer(LayersEnum.Player.ToString());
+                    agent.End();
+                    nvAgent.enabled = false;
+
+                    Dead();
+
+                    RespawnWithDelay(_respawnTime, () =>
+                    {
+                        if (agent.BlackboardReference
+                        .GetVariable<GameObject[]>(_BBPatrolPoints, out var bbVariable))
+                        {
+                            //ランダムな徘徊ポイントを取得
+                            GameObject[] points = bbVariable.Value;
+                            int index = UnityEngine.Random.Range(0, points.Length);
+                            Transform point = points[index].transform;
+
+                            transform.position = point.position; //ポイントにワープ
+                        }
+
+                        rb.excludeLayers = 0; //レイヤーを戻す
+                        nvAgent.enabled = true;
+                        agent.Start();
+                    });
                 }
                 else
                 {
@@ -34,6 +72,33 @@ namespace ChristianGamers.Ingame.Stage
                     player.KnockBack(dir * _knockBackPower, _stunTime);
                 }
             }
+        }
+
+        /// <summary>
+        ///     ディレイしてからリスポーンする
+        /// </summary>
+        /// <param name="delayTime"></param>
+        /// <param name="respawnAction"></param>
+        private async void RespawnWithDelay(float delayTime, Action respawnAction)
+        {
+            try
+            {
+                await Awaitable.WaitForSecondsAsync(delayTime, destroyCancellationToken);
+            }
+            catch (OperationCanceledException) { }
+
+            respawnAction?.Invoke();
+            Respawn();
+        }
+
+        private void Dead()
+        {
+            OnDead?.Invoke();
+        }
+
+        private void Respawn()
+        {
+            OnRespawn?.Invoke();
         }
     }
 }
