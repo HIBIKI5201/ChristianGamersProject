8c50256e306266ea51a0707c5c49e2dfa39e528f
HIBIKI_5201 <takuto5201@gmail.com>
2025-07-20 19:32:51 +0900

    [update]シークエンスのマネージャーを実装

--- Assets/Scripts/Runtime/Ingame/System/IngameSequence.cs
+++ Assets/Scripts/Runtime/Ingame/System/IngameSequence.cs
@@ -1,12 +1,43 @@
+using ChristianGamers.Ingame.Player;
+using SymphonyFrameWork.System;
 using UnityEngine;
+using UnityEngine.Playables;
 
 namespace ChristianGamers.Ingame.Sequence
 {
+    [DefaultExecutionOrder(1000)]
     public class IngameSequence : MonoBehaviour
     {
+        [SerializeField]
+        private PlayableDirector _startTimeLineDirector;
+
         private void Start()
         {
-            Debug.Log("インゲーム開始");
+            //必要な参照を取得
+            IngameTimer timer = ServiceLocator.GetInstance<IngameTimer>();
+            PlayerManager player = ServiceLocator.GetInstance<PlayerManager>();
+
+            //一連のシークエンスのイベントを登録
+            _startTimeLineDirector.stopped += d => HandleStart();
+            timer.OnTimeUp += HandleTimeUp;
+
+            try
+            {
+                _startTimeLineDirector.Play(); //タイムラインを開始
+            }
+            catch { HandleStart(); } //Playで問題が起こったら始める
+
+            void HandleStart()
+            {
+                timer.Play();
+                player.SetActiveInputHandle(true);
+            }
+
+            void HandleTimeUp()
+            {
+                timer.Stop();
+                player.SetActiveInputHandle(false);
+            }
         }
     }
 }
