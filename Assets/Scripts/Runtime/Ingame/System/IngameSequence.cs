231b46cca3e058bbd849620f553627263e2d14b7
HIBIKI_5201 <takuto5201@gmail.com>
2025-07-20 23:16:30 +0900

    [update]BGMを再生する

--- Assets/Scripts/Runtime/Ingame/System/IngameSequence.cs
+++ Assets/Scripts/Runtime/Ingame/System/IngameSequence.cs
@@ -1,5 +1,7 @@
 using ChristianGamers.Ingame.Player;
 using SymphonyFrameWork.System;
+using System;
+using System.Threading;
 using UnityEngine;
 using UnityEngine.Playables;
 
@@ -11,6 +13,10 @@ namespace ChristianGamers.Ingame.Sequence
         [SerializeField]
         private PlayableDirector _startTimeLineDirector;
 
+        [SerializeField]
+        private AudioClip[] _bgms;
+
+        private CancellationTokenSource _bgmCancellationTokenSource;
         private void Start()
         {
             //必要な参照を取得
@@ -31,13 +37,45 @@ namespace ChristianGamers.Ingame.Sequence
             {
                 timer.Play();
                 player.SetActiveInputHandle(true);
+
+                _bgmCancellationTokenSource = new CancellationTokenSource();
+                AudioSource source = AudioManager.GetAudioSource(AudioGroupTypeEnum.BGM.ToString());
+                BGMLoop(source, _bgms, _bgmCancellationTokenSource.Token);
             }
 
             void HandleTimeUp()
             {
                 timer.Stop();
                 player.SetActiveInputHandle(false);
+                _bgmCancellationTokenSource.Cancel();
             }
+        }
+
+        private async void BGMLoop(AudioSource source, AudioClip[] clips, CancellationToken token = default)
+        {
+            source.loop = false;
+
+            int index = 0;
+            while (true)
+            {
+                //新しいクリップに変更
+                source.Stop();
+                source.clip = clips[index];
+                source.Play();
+
+                try
+                {
+                    //再生中は待機
+                    while (source.isPlaying)
+                    {
+                        await Awaitable.NextFrameAsync(token);
+                    }
+                }
+                catch (OperationCanceledException) { break; }
+
+                index = ++index % clips.Length;
+            }
+            source.loop = true;
         }
     }
 }
